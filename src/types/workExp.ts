9c1bd0f1f649a0496c4292e379d0b40569954320
Shayan Haghighi <shayan.haghighi04@gmail.com>
2025-10-10 18:36:13 +0100

    update navbar

---
+++ src/types/workExp.ts
@@ -0,0 +1,9 @@
+export type WorkExp = {
+    id: string;
+    title: string;
+    shortDescription: string;
+    startDate:string;
+    endDate:string;
+    employer?:string;
+    tags: string[];
+}
