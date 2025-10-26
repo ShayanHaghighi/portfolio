33f860918e2878f7efa5851ef282df5895644da8
Shayan Haghighi <shayan.haghighi04@gmail.com>
2025-10-26 23:36:45 +0000

    add themes and minor fixes

--- public/theme.js
+++
@@ -1,8 +0,0 @@
-;(function initTheme() {
-    var theme = localStorage.getItem('theme') || 'light'
-    console.log('dark')
-
-    if (theme === 'dark') {
-        document.documentElement.setAttribute('data-theme','dark')
-    }
-})()
